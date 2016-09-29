7aca5f5ad2709f64773013c21c4c347d80ab34e8
Nguyen Linh <nguyenlinh.nttu@gmail.com>
2016-09-29 17:05:39 +0700

    javacore demo

---
+++ JavaCore/hashmap/Main.java
@@ -0,0 +1,15 @@
+package hashmap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DataHashMap dataHashMap= new DataHashMap();
+		dataHashMap.addItems();
+		dataHashMap.showMapItem();
+		dataHashMap.getValue(2);
+		dataHashMap.checkValue("Nguyễn Văn Linh");
+		dataHashMap.getHashCode();
+	}
+
+}
---
+++ JavaCore/linkedlist/Main.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	DataLinked dataLinked= new DataLinked();
+	dataLinked.addItem();
+	dataLinked.showList();
+	dataLinked.addFirstItem("FistName");
+	dataLinked.addItemWithPosition(2,"Nam");
+	dataLinked.changeItemWithPos(3,"Việt");
+	System.out.println("LinkedList sau khi thay đổi là:");
+	dataLinked.showList();
+	}
+}
